680bfe86f1d16cc0cd476929eaf76ee9ecb14a4c
George Tada <george.6809@gmail.com>
2018-04-07 15:18:19 +0900

    JSON API を返すようにした

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const PORT = process.env.PORT || 5000
 express()
   .get('/', (req, res) => res.send('json mock for test'))
   .get('/comments', (req, res) =>
-    res.send(
-      '[{"id":"0","comment":"I love you."},{"id":"1","comment":"You are my love."},{"id":"2","comment":"I will be with you forever."},{"id":"3","comment":"Nothing\'s gonna change my love for you."},{"id":"4","comment":"Go fuck yourself!"}]'
+    res.json(
+      [
+        {id: '0', comment: 'I love you.'},
+        {id: '1', comment: 'You are great.'},
+        {id: '3', comment: 'It\'s a great sounds'},
+        {id: '4', comment: 'How marvelous'}
+      ]
     )
   )
-  .post('/comment', (req, res) =>
-    res.send(
-      '[{"id":"0","comment":"I love you."},{"id":"1","comment":"You are my love."},{"id":"2","comment":"I will be with you forever."},{"id":"3","comment":"Nothing\'s gonna change my love for you."},{"id":"4","comment":"Go fuck yourself!"}]'
+  .post('/comments', (req, res) =>
+    res.json(
+      [
+        {id: '0', comment: 'I love you.'},
+        {id: '1', comment: 'You are great.'},
+        {id: '3', comment: 'It\'s a great sounds'},
+        {id: '4', comment: 'How marvelous'}
+      ]
     )
   )
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
