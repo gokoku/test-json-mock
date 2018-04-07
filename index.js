bd12801b7e1c34e66166cc8a4310190ccea8fd2d
George Tada <george.6809@gmail.com>
2018-04-07 13:15:57 +0900

    mock ということで/の文字を変えた

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.send('json api for test'))
+  .get('/', (req, res) => res.send('json mock for test'))
   .get('/comment', (req, res) =>
     res.send(
       '[{"id":"0","comment":"I love you."},{"id":"1","comment":"You are my love."},{"id":"2","comment":"I will be with you forever."},{"id":"3","comment":"Nothing\'s gonna change my love for you."},{"id":"4","comment":"Go fuck yourself!"}]'
