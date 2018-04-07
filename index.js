4d659eac5b8a63907ed71119e0fc3757862c4e7e
George Tada <george.6809@gmail.com>
2018-04-07 16:15:25 +0900

    ヘッダにcros-Origin Resource Sharingを許可する

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
+var app = express()
 
-express()
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Cache-Control,access_token');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+/**
+ \* OPTIONSメソッドの実装
+ \*/
+app.options('*', function (req, res) {
+  res.sendStatus(200);
+});
+
+
+app
   .get('/', (req, res) => res.send('json mock for test'))
-  .get('/comments', (req, res) =>
+  .get('/comments', (req, res) => {
     res.json(
       [
         {id: '0', comment: 'I love you.'},
@@ -12,7 +29,7 @@ express()
         {id: '4', comment: 'How marvelous'}
       ]
     )
-  )
+  })
   .post('/comments', (req, res) =>
     res.json(
       [
