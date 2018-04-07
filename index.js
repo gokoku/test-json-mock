ac7151bba4b878fc27633a52821ee3f79f9c6b79
George Tada <george.6809@gmail.com>
2018-04-07 13:06:23 +0900

    テスト用JSONモックを吐くAPIにした

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.send('json api for test'))
+  .get('/comment', (req, res) =>
+    res.send(
+      '[{"id":"0","comment":"I love you."},{"id":"1","comment":"You are my love."},{"id":"2","comment":"I will be with you forever."},{"id":"3","comment":"Nothing\'s gonna change my love for you."},{"id":"4","comment":"Go fuck yourself!"}]'
+    )
+  )
+  .post('/comment', (req, res) =>
+    res.send(
+      '[{"id":"0","comment":"I love you."},{"id":"1","comment":"You are my love."},{"id":"2","comment":"I will be with you forever."},{"id":"3","comment":"Nothing\'s gonna change my love for you."},{"id":"4","comment":"Go fuck yourself!"}]'
+    )
+  )
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
